e8ff71473a2abc9078fc790c0e6ee71c70848ebc
SiddheshKukade <littlelord124@gmail.com>
2024-02-19 12:53:56 +0530

    add: shufflearray function

--- src/utlis/utils.js
+++ src/utlis/utils.js
@@ -10,3 +10,10 @@ export const getFirstTypeName = (parameters) => {
     return typeName
   }
 }
+export const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+} 
