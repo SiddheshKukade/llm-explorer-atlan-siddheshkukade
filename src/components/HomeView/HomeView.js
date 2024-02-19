f9cf8e0c6f53de9550e8ba5c6bf4079ef972404c
SiddheshKukade <littlelord124@gmail.com>
2024-02-19 12:55:33 +0530

    add: homeview

---
+++ src/components/HomeView/HomeView.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import { Box, Flex, useColorModeValue } from '@chakra-ui/react'
+import { Sidebar } from '../Sidebar/Sidebar'
+import ModelsView from '../ModelsView/ModelsView'
+const HomeView = () => {
+  return (
+    <Flex width={"100vw"} height={"100vh"} direction={"column"}>
+      <Navbar />
+
+      <Box minH={"100vh"} display={"flex"} flexDirection={"row"} bg={useColorModeValue("gray.100", 'gray.900')}>
+        <Sidebar
+          pl={20}
+          display={{ base: 'none', lg: 'block' }}
+          minW={"30vw"}
+        />
+        <ModelsView />
+
+      </Box>
+
+    </Flex>
+  )
+}
+
+export default HomeView
