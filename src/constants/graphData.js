563393ba72a3e72759274a2c310c5fb810484942
SiddheshKukade <littlelord124@gmail.com>
2024-02-19 12:54:10 +0530

    add: constants

---
+++ src/constants/graphData.js
@@ -0,0 +1,59 @@
+export const graphData = [
+    {
+        name: '',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: '',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: '',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: '',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: '',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+    {
+        name: '',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: '',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+
+];
+export const chatbotResponses = [
+    "Hello! How can I assist you today?",
+    "Hi there! What can I help you with?",
+    "Greetings! Feel free to ask me anything.",
+    "Hey! I'm here to answer your questions.",
+    "Hello! Nice to meet you. What brings you here?",
+    "Hi! How can I make your day better?",
+    "Greetings! Ready to chat. What's on your mind?",
+    "Hello! I'm your friendly chatbot. What do you need help with?",
+    "Hey there! I'm here to provide information and support.",
+    "Hello! Feel free to start a conversation or ask a question."
+  ];
+  
+  
